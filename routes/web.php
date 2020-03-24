44bd959e239ceaa1904a0b6b007337bf1090f73b
brainthief <vytautas1983@gmail.com>
2020-03-24 19:51:11 +0200

    meniu

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
 
 Route::get('/services', function () {
     return view('services');
-});
+})->name('services');
