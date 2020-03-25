33c8e0fdadd28e7eb878a7db334fd68931c148ac
brainthief <vytautas1983@gmail.com>
2020-03-25 21:11:13 +0200

    Products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,6 +11,8 @@ class HomeController extends Controller
     public function index()
     {
         $categories = Category::all();
-        return view('index', compact('categories'));
+        // $products = Product::all();
+        $products = Product::with('category')->get();
+        return view('index', compact('categories', 'products'));
     }
 }
